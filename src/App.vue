b7520730459c9f5d05a0312d7a6662ae57d44dbd
mathsalam <mathsalam25@gmail.com>
2026-06-03 13:28:07 +0700

    change doc to hahahaha

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,7 @@
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+hahahahahahaz  </p>
 </template>
 
 <style scoped></style>
