d345d3a8a7452ee102cbb03c55a8fd0d9c154f92
mathsalam <mathsalam25@gmail.com>
2026-06-05 10:39:02 +0700

    setup bootstrap Sign In and Sing Up style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "admin-lte/dist/js/adminlte.min.js";
 
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+createApp(App).use(router).mount("#app");
